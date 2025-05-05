76d7566d5c82d4c0d4961a464f93f345078be774
Abdullah-Farhan <102550496+Abdullah-Farhan@users.noreply.github.com>
2025-05-05 15:03:07 +0500

    feat: navigate to /dashboard by default

--- src/components/sections/dashboard/page.jsx
+++ src/components/sections/dashboard/page.jsx
@@ -1,8 +1,11 @@
+"use client";
 import EventScheduler from "@/components/calenders/EventScheduler";
 import { ExternalLink } from "lucide-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const page = () => {
+  const [selectedProgress, setSelectedProgress] = useState("90 Days");
   return (
     <div className="px-8 py-5">
       <h1 className="text-3xl font-light">Dashboard</h1>
@@ -21,11 +24,32 @@ const page = () => {
                 View All <ExternalLink className="w-4 h-4 ml-1" />
               </Link>
             </div>
-              <div className="w-full flex space-x-2 rounded bg-gray-300">
-                <button>90 Day</button>
-                <button>Graduation</button>
-                <button>Kitchen Use</button>
-              </div>
+            <div className="w-full flex space-x-2 rounded bg-gray-200 p-1">
+              <button
+                className={`w-1/3 rounded cursor-pointer ${
+                  selectedProgress === "90 Days" ? "bg-white" : ""
+                }`}
+                onClick={()=>setSelectedProgress("90 Days")}
+              >
+                90 Day
+              </button>
+              <button
+                className={`w-1/3 rounded cursor-pointer ${
+                  selectedProgress === "Graduation" ? "bg-white" : ""
+                }`}
+                onClick={()=>setSelectedProgress("Graduation")}
+              >
+                Graduation
+              </button>
+              <button
+                className={`w-1/3 rounded cursor-pointer ${
+                  selectedProgress === "Kitchen" ? "bg-white" : ""
+                }`}
+                onClick={()=>setSelectedProgress("Kitchen")}
+              >
+                Kitchen Use
+              </button>
+            </div>
           </div>
         </div>
       </div>
